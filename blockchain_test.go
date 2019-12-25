6bc8c1ca8dc104d3993883046a4bfd85202e0736
EvanXzj <2686465170@qq.com>
2019-12-25 20:04:41 +0800

    part 4 done

--- blockchain_test.go
+++ blockchain_test.go
@@ -3,9 +3,11 @@ package main
 import "testing"
 
 func TestNewGenesisBlock(t *testing.T) {
-	block := NewGenesisBlock()
+	coinbase := NewCoinbaseTX("xyz", "Genesis Block")
 
-	if string(block.Data) != "Genesis Block" {
+	block := NewGenesisBlock(coinbase)
+
+	if string(block.Transactions[0].Vin[0].ScriptSig) != "Genesis Block" {
 		t.Error("TestNewGenesisBlock failed")
 	}
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/boltdb/bolt v1.3.1
 	github.com/coreos/bbolt v1.3.3
+	go.uber.org/zap v1.13.0
 	golang.org/x/sys v0.0.0-20191210023423-ac6580df4449 // indirect
 )
